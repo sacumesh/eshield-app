5a25c6167318eff062238022464c963aca95c95d
Sachiththa Bandaranayake <sacume3@gmail.com>
2023-07-15 16:14:47 +0200

    :sparkles: setup login and basic routing

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userContextMenu = {
+    tag: 'user-context-menu',
+    items: [{ title: 'Profile' }, { title: 'Log out' }],
+  };
+  constructor(
+    private _sidebarService: NbSidebarService,
+    private _menuService: NbMenuService
+  ) {}
 
+  ngOnInit(): void {
+    this._menuService
+      .onItemClick()
+      .pipe(
+        filter(({ tag }) => tag === 'user-context-menu'),
+        map(({ item: { title } }) => title)
+      )
+      .subscribe(console.log);
+  }
+
+  toggleSidebar(): boolean {
+    this._sidebarService.toggle(true, 'menu-sidebar');
+    // this.layoutService.changeLayoutSize();
+
+    return false;
+  }
+
+  navigateHome() {
+    this._menuService.navigateHome();
+    return false;
+  }
 }
