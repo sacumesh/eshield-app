1fc7e2fb682ed68d20545dfad08504c8f8341411
Sachiththa Bandaranayake <sacume3@gmail.com>
2023-07-20 15:45:33 +0200

    :sparkles: Create courses page

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  flipped = false;
+  onExamClick() {
+    console.log('exam');
+  }
 
+  toggleView() {
+    this.flipped = !this.flipped;
+  }
 }
